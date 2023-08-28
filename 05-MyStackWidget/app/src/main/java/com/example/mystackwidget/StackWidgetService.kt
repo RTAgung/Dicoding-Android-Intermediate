755d4f63f268b5ad0313ca253bf97015604706ec
Rama Tri Agung <ramatriagung91@gmail.com>
2023-08-28 11:06:11 +0700

    finish(05-StackWidget)

---
+++ 05-MyStackWidget/app/src/main/java/com/example/mystackwidget/StackWidgetService.kt
@@ -0,0 +1,9 @@
+package com.example.mystackwidget
+
+import android.content.Intent
+import android.widget.RemoteViewsService
+
+class StackWidgetService : RemoteViewsService() {
+    override fun onGetViewFactory(p0: Intent?): RemoteViewsFactory =
+        StackRemoteViewsFactory(this.applicationContext)
+}
