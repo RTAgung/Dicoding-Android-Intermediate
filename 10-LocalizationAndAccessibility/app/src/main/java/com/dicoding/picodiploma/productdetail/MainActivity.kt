1bdfcfbea04e58e4a0895c80446fbfd94f39cb1d
Rama Tri Agung <ramatriagungwork@gmail.com>
2023-09-03 11:58:50 +0700

    finish(10-Formatting)

--- 10-LocalizationAndAccessibility/app/src/main/java/com/dicoding/picodiploma/productdetail/MainActivity.kt
+++ 10-LocalizationAndAccessibility/app/src/main/java/com/dicoding/picodiploma/productdetail/MainActivity.kt
@@ -19,11 +19,11 @@ class MainActivity : AppCompatActivity() {
 
         setupView()
         setupAction()
+        setupData()
     }
 
     private fun setupView() {
-        @Suppress("DEPRECATION")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        @Suppress("DEPRECATION") if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
         } else {
             window.setFlags(
@@ -37,6 +37,27 @@ class MainActivity : AppCompatActivity() {
     private fun setupAction() {
         binding.settingImageView.setOnClickListener {
             startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+        }
+    }
+
+    private fun setupData() {
+        val repository = RemoteDataSource(this)
+        val product = repository.getDetailProduct().apply {
+            binding.apply {
+                previewImageView.setImageResource(image)
+                nameTextView.text = name
+                storeTextView.text = store
+                colorTextView.text = color
+                sizeTextView.text = size
+                descTextView.text = desc
+                priceTextView.text = price.withCurrencyFormat()
+                dateTextView.text = getString(R.string.dateFormat, date.withDateFormat())
+                ratingTextView.text = getString(
+                    R.string.ratingFormat,
+                    rating.withNumberingFormat(),
+                    countRating.withNumberingFormat()
+                )
+            }
         }
     }
 }
