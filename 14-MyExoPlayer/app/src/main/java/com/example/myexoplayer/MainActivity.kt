4e8351cc24d8960adfcd31c8be1475e114275cbf
Rama Tri Agung <ramatriagungwork@gmail.com>
2023-09-03 20:04:33 +0700

    finish(14-MyExoPlayer) : Adding Notification while playing music

--- 14-MyExoPlayer/app/src/main/java/com/example/myexoplayer/MainActivity.kt
+++ 14-MyExoPlayer/app/src/main/java/com/example/myexoplayer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myexoplayer
 
+import android.content.ComponentName
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.WindowCompat
@@ -7,7 +8,10 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
 import com.example.myexoplayer.databinding.ActivityMainBinding
+import com.google.common.util.concurrent.MoreExecutors
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,19 +20,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val videoItem =
-            MediaItem.fromUri("https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4")
-        val audioItem =
-            MediaItem.fromUri("https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3")
-
-        val player = ExoPlayer.Builder(this).build().also { exoPlayer ->
-            exoPlayer.setMediaItem(videoItem)
-            exoPlayer.addMediaItem(audioItem)
-            exoPlayer.prepare()
-        }
-
-        binding.playerView.player = player
 
         hideSystemUI()
     }
@@ -41,5 +32,16 @@ class MainActivity : AppCompatActivity() {
                 WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        val sessionToken = SessionToken(this, ComponentName(this, PlaybackService::class.java))
+        val controllerFuture = MediaController.Builder(this, sessionToken).buildAsync()
+        controllerFuture.addListener(
+            { binding.playerView.player = controllerFuture.get() },
+            MoreExecutors.directExecutor()
+        )
+    }
+
 
 }
