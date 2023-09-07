59173baaa7a699b04c4014c8cc3bb3e74ccc3bdb
Rama Tri Agung <ramatriagungwork@gmail.com>
2023-09-07 14:00:55 +0700

    do(15-CameraX) : finish intent gallery

--- 15-CameraX/app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
+++ 15-CameraX/app/src/main/java/com/dicoding/picodiploma/mycamera/MainActivity.kt
@@ -2,6 +2,7 @@ package com.dicoding.picodiploma.mycamera
 
 import android.Manifest
 import android.content.Intent
+import android.content.Intent.ACTION_GET_CONTENT
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -57,6 +58,18 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg = result.data?.data as Uri
+            selectedImg.let { uri ->
+                val myFile = uriToFile(uri, this@MainActivity)
+                binding.previewImageView.setImageURI(uri)
+            }
+        }
+    }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
@@ -103,7 +116,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startGallery() {
-        Toast.makeText(this, "Fitur ini belum tersedia", Toast.LENGTH_SHORT).show()
+        val intent = Intent()
+        intent.action = ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser)
     }
 
     private fun startTakePhoto() {
