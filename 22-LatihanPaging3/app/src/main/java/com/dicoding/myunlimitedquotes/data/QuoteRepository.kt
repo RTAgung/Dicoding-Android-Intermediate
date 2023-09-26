c149f20bc31691c1d70b0f9bd686d26e31efb3da
Rama Tri Agung <ramatriagungwork@gmail.com>
2023-09-26 19:26:56 +0700

    do(22-LatihanPaging3): Paging3 RemoteMediator

--- 22-LatihanPaging3/app/src/main/java/com/dicoding/myunlimitedquotes/data/QuoteRepository.kt
+++ 22-LatihanPaging3/app/src/main/java/com/dicoding/myunlimitedquotes/data/QuoteRepository.kt
@@ -1,6 +1,7 @@
 package com.dicoding.myunlimitedquotes.data
 
 import androidx.lifecycle.LiveData
+import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
@@ -18,12 +19,15 @@ class QuoteRepository(
 //    }
 
     fun getQuote(): LiveData<PagingData<QuoteResponseItem>> {
+        @OptIn(ExperimentalPagingApi::class)
         return Pager(
             config = PagingConfig(
                 pageSize = 5
             ),
+            remoteMediator = QuoteRemoteMediator(quoteDatabase, apiService),
             pagingSourceFactory = {
-                QuotePagingSource(apiService)
+//                QuotePagingSource(apiService)
+                quoteDatabase.quoteDao().getAllQuote()
             }
         ).liveData
     }
