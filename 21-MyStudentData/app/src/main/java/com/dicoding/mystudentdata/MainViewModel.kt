a58fe39c2349e255b62a6ec5603d49b7fca01bfb
Rama Tri Agung <ramatriagungwork@gmail.com>
2023-09-26 10:41:28 +0700

    do(21-MyStudentData): Pre-populate data in Room Db

--- 21-MyStudentData/app/src/main/java/com/dicoding/mystudentdata/MainViewModel.kt
+++ 21-MyStudentData/app/src/main/java/com/dicoding/mystudentdata/MainViewModel.kt
@@ -3,30 +3,22 @@ package com.dicoding.mystudentdata
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
 import com.dicoding.mystudentdata.database.Student
 import com.dicoding.mystudentdata.database.StudentAndUniversity
 import com.dicoding.mystudentdata.database.StudentWithCourse
 import com.dicoding.mystudentdata.database.UniversityAndStudent
-import kotlinx.coroutines.launch
 
 class MainViewModel(private val studentRepository: StudentRepository) : ViewModel() {
 
-    init {
-        insertAllData()
-    }
-
     fun getAllStudent(): LiveData<List<Student>> = studentRepository.getAllStudent()
+    fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>> =
+        studentRepository.getAllStudentAndUniversity()
 
-    fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>> = studentRepository.getAllStudentAndUniversity()
+    fun getAllUniversityAndStudent(): LiveData<List<UniversityAndStudent>> =
+        studentRepository.getAllUniversityAndStudent()
 
-    fun getAllUniversityAndStudent(): LiveData<List<UniversityAndStudent>> = studentRepository.getAllUniversityAndStudent()
-
-    fun getAllStudentWithCourse(): LiveData<List<StudentWithCourse>> = studentRepository.getAllStudentWithCourse()
-
-    private fun insertAllData() = viewModelScope.launch {
-        studentRepository.insertAllData()
-    }
+    fun getAllStudentWithCourse(): LiveData<List<StudentWithCourse>> =
+        studentRepository.getAllStudentWithCourse()
 }
 
 class ViewModelFactory(private val repository: StudentRepository) : ViewModelProvider.Factory {
--- 21-MyStudentData/app/src/main/java/com/dicoding/mystudentdata/StudentRepository.kt
+++ 21-MyStudentData/app/src/main/java/com/dicoding/mystudentdata/StudentRepository.kt
@@ -10,20 +10,10 @@ import com.dicoding.mystudentdata.helper.InitialDataSource
 
 class StudentRepository(private val studentDao: StudentDao) {
     fun getAllStudent(): LiveData<List<Student>> = studentDao.getAllStudent()
-
     fun getAllStudentAndUniversity(): LiveData<List<StudentAndUniversity>> =
         studentDao.getAllStudentAndUniversity()
-
     fun getAllUniversityAndStudent(): LiveData<List<UniversityAndStudent>> =
         studentDao.getAllUniversityAndStudent()
-
     fun getAllStudentWithCourse(): LiveData<List<StudentWithCourse>> =
         studentDao.getAllStudentWithCourse()
-
-    suspend fun insertAllData() {
-        studentDao.insertStudent(InitialDataSource.getStudents())
-        studentDao.insertUniversity(InitialDataSource.getUniversities())
-        studentDao.insertCourse(InitialDataSource.getCourses())
-        studentDao.insertCourseStudentCrossRef(InitialDataSource.getCourseStudentRelation())
-    }
 }
