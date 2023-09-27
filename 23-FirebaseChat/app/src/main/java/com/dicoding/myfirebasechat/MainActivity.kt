df566aac1cf0efc4f46868d8b9b7f66dcc7b9aa0
Rama Tri Agung <ramatriagungwork@gmail.com>
2023-09-27 11:31:40 +0700

    do(23-FirebaseChat): Firebase Auth

--- 23-FirebaseChat/app/src/main/java/com/dicoding/myfirebasechat/MainActivity.kt
+++ 23-FirebaseChat/app/src/main/java/com/dicoding/myfirebasechat/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.dicoding.myfirebasechat
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.myfirebasechat.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        auth = Firebase.auth
+        val firebaseUser = auth.currentUser
+
+        if (firebaseUser == null) {
+            // Not signed in, launch the Login activity
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+            return
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -29,11 +53,20 @@ class MainActivity : AppCompatActivity() {
                 signOut()
                 true
             }
+
             else -> super.onOptionsItemSelected(item)
         }
     }
 
-    private fun signOut() {
+    private fun moveToLoginPage() {
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
+    }
 
+    private fun signOut() {
+        auth.signOut()
+        googleSignInClient.signOut().addOnCompleteListener {
+            moveToLoginPage()
+        }
     }
 }
