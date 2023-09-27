5009e88b0de958e91ac15f16cec6b7afcd85fe4f
Rama Tri Agung <ramatriagungwork@gmail.com>
2023-09-27 12:13:01 +0700

    do(23-FirebaseChat): Chat Realtime Db

--- 23-FirebaseChat/app/build.gradle.kts
+++ 23-FirebaseChat/app/build.gradle.kts
@@ -42,9 +42,17 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
-    implementation("com.google.android.gms:play-services-auth:20.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+
+    // auth
+    implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+    // realtime db
+    implementation("com.google.firebase:firebase-database-ktx:20.2.2") // push data
+    implementation("com.firebaseui:firebase-ui-database:8.0.0") // get data adapter
 }
--- 23-FirebaseChat/app/src/main/java/com/dicoding/myfirebasechat/MainActivity.kt
+++ 23-FirebaseChat/app/src/main/java/com/dicoding/myfirebasechat/MainActivity.kt
@@ -4,20 +4,28 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dicoding.myfirebasechat.databinding.ActivityMainBinding
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var db: FirebaseDatabase
+    private lateinit var adapter: FirebaseMessageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +47,51 @@ class MainActivity : AppCompatActivity() {
             finish()
             return
         }
+
+        db = Firebase.database
+        val messagesRef = db.reference.child(MESSAGES_CHILD)
+
+        binding.sendButton.setOnClickListener {
+            val friendlyMessage = Message(
+                binding.messageEditText.text.toString(),
+                firebaseUser.displayName.toString(),
+                firebaseUser.photoUrl.toString(),
+                Date().time
+            )
+            messagesRef.push().setValue(friendlyMessage) { error, _ ->
+                if (error != null) {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.send_error) + error.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(this, getString(R.string.send_success), Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+            binding.messageEditText.setText("")
+        }
+
+        val manager = LinearLayoutManager(this)
+        manager.stackFromEnd = true
+        binding.messageRecyclerView.layoutManager = manager
+
+        val options = FirebaseRecyclerOptions.Builder<Message>()
+            .setQuery(messagesRef, Message::class.java)
+            .build()
+        adapter = FirebaseMessageAdapter(options, firebaseUser.photoUrl.toString())
+        binding.messageRecyclerView.adapter = adapter
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        adapter.startListening()
+    }
+
+    public override fun onPause() {
+        adapter.stopListening()
+        super.onPause()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -68,5 +121,9 @@ class MainActivity : AppCompatActivity() {
         googleSignInClient.signOut().addOnCompleteListener {
             moveToLoginPage()
         }
+    }
+
+    companion object {
+        const val MESSAGES_CHILD = "messages"
     }
 }
